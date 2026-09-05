76a6caf5bf045eaa9452c5dbfe5fad65b2a968c3
Andrew Ferreira <andrewdferreira@gmail.com>
2026-09-05 16:26:29 +0000

    Criação da pasta src e classe Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {    
+        System.out.println("==============================");
+        System.out.println("      SISTEMA DE PROJETOS     ");
+        System.out.println("==============================");
+        System.out.println("Projeto Portifolio Acadêmico");
+        System.out.println("Desenvolvido em Java");
+        System.out.println("Versao: 1.0");
+    }
+}
